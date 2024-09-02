e18a1f109bccff373fa0f76874545eed996382d3
goalTosin <gunsin.goal@gmail.com>
2024-09-02 14:36:35 +0100

    Cleanup + Fix implement camera rotation + Allow starfield rotation

--- js/pages/play_page.js
+++ js/pages/play_page.js
@@ -2,6 +2,7 @@ import Planet from "../classes/planet.js";
 import Rocket from "../classes/rocket.js";
 import StarField from "../classes/starfield.js";
 import lerp from "../utils/lerp.js";
+import randomItem from "../utils/randomItem.js";
 import Page from "./page.js";
 
 class PlayPage extends Page {
@@ -19,14 +20,9 @@ class PlayPage extends Page {
   exit() {
     this.exitToPage("home");
   }
-  onSizeChange() {}
   init() {
-    this.rocket = new Rocket(
-      0, // 7071
-      0
-      //  -7071
-    );
-    let n = 20;
+    this.rocket = new Rocket(0, 0);
+    let n = 3;
     let originX = 0;
     let originY = 0;
     /**
@@ -34,10 +30,7 @@ class PlayPage extends Page {
      */
     this.planets = Array.from({ length: n }, (k, i) => {
       let a = (i / n) * Math.PI * 2;
-      let d =
-        //  rrand(
-        90000;
-      // );
+      let d = 90000;
       return new Planet(Math.cos(a) * d + originX, Math.sin(a) * d + originY);
     });
     let ri = Math.floor(Math.random(this.planets.length));
@@ -47,13 +40,11 @@ class PlayPage extends Page {
       return;
     });
     this.destinationPlanet = this.planets[ri + 1];
-    //  randomItem(
-    // this.planets.filter((k) => k !== this.launchOriginPlanet)
-    // );
+
     this.camera = {
       x: this.rocket.x,
       y: this.rocket.y,
-      rotation: 0,
+      rotation: this.rocket.rotation,
       moveBy(x, y) {
         this.x += x;
         this.y += y;
@@ -68,15 +59,12 @@ class PlayPage extends Page {
       this.camera.x,
       this.camera.y,
       this.canvas.width,
-      this.canvas.height
+      this.canvas.height,
+      this.camera.rotation
     );
-    
+
     this.handleKeyupHandler = this.handleKeyup.bind(this);
     this.handleKeydownHandler = this.handleKeydown.bind(this);
-    // for (let i = 0; i < 4; i++) {
-    //   const planet = this.planets[i];
-
-    // }
   }
   createEvents() {
     addEventListener("keydown", this.handleKeydownHandler);
@@ -95,42 +83,32 @@ class PlayPage extends Page {
 
   handleKeyup(e) {
     this.handleKeyed(e);
-    // if (e.code === "KeyW" || e.key === " " || e.key === "ArrowUp") {
-    //   this.rocket.thrusting = false;
-    // }
   }
 
   handleKeyed(e) {
     this.keysDown[e.key] = e.type === "keydown";
     this.keysDown[e.code] = e.type === "keydown";
-    // console.log(this.keysDown);
   }
   update(dt) {
-    this.starfield.update(this.ctx, this.camera);
-    if (this.started) {
-      this.camera.x = this.rocket.x;
-      this.camera.y = this.rocket.y;
-      this.started = false;
-    }
     this.ctx.save();
-    this.ctx.translate(
-      this.canvas.width / 2 - this.camera.x,
-      this.canvas.height / 2 - this.camera.y
-    );
-    this.ctx.rotate(this.camera.rotation);
-    // this.ctx.scale(0.3, 0.3);
-    // console.log();
+    this.ctx.translate(this.canvas.width / 2, this.canvas.height / 2);
+    this.ctx.rotate(-this.camera.rotation);
+    this.ctx.translate(-this.camera.x, -this.camera.y);
+    this.starfield.update(this.ctx, this.camera);
     if (this.rocket.thrusting && this.rocket.fuel > 0) {
       this.rocket.thrust(dt);
     }
     if (this.keysDown["KeyD"] || this.keysDown["ArrowRight"]) {
       this.rocket.turnRight(dt);
-    }
-    if (this.keysDown["KeyA"] || this.keysDown["ArrowLeft"]) {
+    } else if (this.keysDown["KeyA"] || this.keysDown["ArrowLeft"]) {
       this.rocket.turnLeft(dt);
+    } else {
+      this.camera.rotation +=
+        0.01 * ((this.rocket.rotation % (Math.PI * 2)) - this.camera.rotation);
     }
-    // this.drawStars();
+
     this.planets.forEach((p) => p.draw(this.ctx));
+
     this.rocket.move(dt);
     this.rocket.draw(
       this.ctx,
@@ -145,28 +123,9 @@ class PlayPage extends Page {
       )
     );
 
-    // if (this.rocket.x + this.rocket.width / 2 >= this.canvas.width / 2) {
-    //   this.rocket.x = this.canvas.width / 2 - this.rocket.width / 2;
-    //   this.rocket.vx = 0;
-    // }
-    // if (this.rocket.x - this.rocket.width / 2 <= -this.canvas.width / 2) {
-    //   this.rocket.x = -this.canvas.width / 2 + this.rocket.width / 2;
-    //   this.rocket.vx = 0;
-    // }
-    // if (this.rocket.y + this.rocket.height / 2 >= this.canvas.height / 2) {
-    //   this.rocket.y = this.canvas.height / 2 - this.rocket.height / 2;
-    //   this.rocket.vy = 0;
-    // }
-    // if (this.rocket.y - this.rocket.height / 2 <= -this.canvas.height / 2) {
-    //   this.rocket.y = -this.canvas.height / 2 + this.rocket.height / 2;
-    //   this.rocket.vy = 0;
-    // }
-    // let ocX = this.camera.x;
-    // let ocY = this.camera.y;
-    // this.rocketY
-    // this.camera.x = this.rocket.x;
-    // this.camera.y = this.rocket.y;
-    let factor = 0.1;
+    // this.camera.x += 0.1 * (this.rocket.x + this.rocket.vx - this.camera.x);
+    // this.camera.y += 0.1 * (this.rocket.y + this.rocket.vy - this.camera.y);
+    let factor = 0.1 * dt;
     let throwFactor = 20;
     // let throwFactor = 1 / (factor * 0.6); // * Math.min(this.canvas.width, this.canvas.height) / 600
     this.camera.x = lerp(
@@ -202,24 +161,14 @@ class PlayPage extends Page {
     this.ctx.restore();
     this.rocket.drawDashboard(this.ctx);
   }
-  // createStar(x, y, vx, vy) {
-  //   return { x, y, vx, vy };
-  // }
-  // drawStars() {
-  //   for (let i = 0; i < this.stars.length; i++) {
-  //     const s = this.stars[i];
-  //     this.ctx.beginPath();
-  //     this.ctx.arc(s.x, s.y, 2, 0, Math.PI * 2);
-  //     this.ctx.fillStyle = "white";
-  //     this.ctx.fill();
-  //   }
-  // }
 
-  // create a method that loops over all the planets and if the rocket is in range of the planet pull the rocket towards the planet
   pullRocketToPlanets() {
     for (let i = 0; i < this.planets.length; i++) {
       const planet = this.planets[i];
       if (planet.isRocketApproaching(this.rocket)) {
+        if (planet === this.destinationPlanet) {
+          this.destinationPlanet = randomItem(this.planets);
+        }
         planet.pullRocketToCenter(this.rocket);
       }
     }
